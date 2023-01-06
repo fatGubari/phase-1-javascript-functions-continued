d6448882091ed16a96176bbebdafa344e0f30a5e
fatima abdullah <fatimajubary@gmail.com>
2023-01-06 23:21:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+saturdayFun()
+saturdayFun('bathe my dog')
+function saturdayFun(value = 'roller-skate'){
+    return "This Saturday, I want to "+value+"!"
+}
+
+mondayWork()
+mondayWork("work from home")
+function mondayWork(value = 'go to the office'){
+    return "This Monday, I will "+value +"."
+}
+
+console.log(wrapAdjective("*")("a hard worker"))
+function wrapAdjective(value) {
+    const innerFunction = function (emphatic) {
+      return `You are ${value}${emphatic}${value}!`;
+    };
+    return innerFunction;
+  }
